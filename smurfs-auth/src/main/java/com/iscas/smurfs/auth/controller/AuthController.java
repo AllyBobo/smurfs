8fcc21c397d46c91dc105be30e82ce85b78cc353
AllyBoBo <linglan00@qq.com>
2018-09-02 15:42:21 +0800

    login

--- smurfs-auth/src/main/java/com/iscas/smurfs/auth/controller/AuthController.java
+++ smurfs-auth/src/main/java/com/iscas/smurfs/auth/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.iscas.smurfs.auth.controller;
 
 import com.iscas.smurfs.auth.common.JWTUtils;
+import com.iscas.smurfs.auth.entity.JwtAuthenticationRequest;
 import com.iscas.smurfs.auth.entity.UserInfo;
 import com.iscas.smurfs.auth.service.IAuthService;
 import com.iscas.smurfs.common.entity.ResponseData;
@@ -14,6 +15,14 @@ public class AuthController {
 
     @Autowired
     private IAuthService authService;
+
+    @RequestMapping(value = "token", method = RequestMethod.POST)
+    public ResponseData createAuthenticationToken(
+            @RequestBody JwtAuthenticationRequest authenticationRequest) throws Exception {
+        final String token = authService.login(authenticationRequest);
+        return ResponseData.ok(token);
+    }
+
 
 
 
