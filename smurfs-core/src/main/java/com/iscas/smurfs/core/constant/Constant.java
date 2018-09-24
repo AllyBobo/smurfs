d382fd27c8f322688880aaae4d5811870c417a34
AllyBoBo <linglan00@qq.com>
2018-09-24 19:46:14 +0800

    login

---
+++ smurfs-core/src/main/java/com/iscas/smurfs/core/constant/Constant.java
@@ -0,0 +1,15 @@
+package com.iscas.smurfs.core.constant;
+
+/**
+ * description:
+ *
+ * @author lee
+ * @date 2018/9/23
+ */
+public class Constant {
+    public static int PW_ENCORDER_SALT = 12;
+
+    public static String TOKEN_HEADER = "Authorization";
+    public static final String JWT_KEY_USER_ID = "userId";
+    public static final String JWT_KEY_NAME = "name";
+}
---
+++ smurfs-core/src/main/java/com/iscas/smurfs/core/entity/JWTInfo.java
@@ -0,0 +1,20 @@
+package com.iscas.smurfs.core.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * description:
+ *
+ * @author lee
+ * @date 2018/9/23
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JWTInfo {
+    private String username;
+    private String userId;
+    private String name;
+}
